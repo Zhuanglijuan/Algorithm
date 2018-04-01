34e145d452b18da16ec40d8ee35858dbfa18be9c
Zhuanglijuan <zhuangljwork@163.com>
2018-04-01 19:26:33 +0800

    Path-Compression

--- UnionFind/Main.java
+++ UnionFind/Main.java
@@ -4,8 +4,8 @@ public class Main {
     // 对比UF1和UF2的时间性能
     public static void main(String[] args) {
 
-        // 使用10000的数据规模
-        int n = 10000;
+        // 使用100000的数据规模
+        int n = 100000;
 
         // 虽然isConnected只需要O(1)的时间, 但由于union操作需要O(n)的时间
         // 总体测试过程的算法复杂度是O(n^2)的
@@ -20,5 +20,9 @@ public class Main {
         // 对于UF3来说, 其时间性能依然是O(n*h)的, h为并查集表达的树的最大高度
         // 但由于UF3能更高概率的保证树的平衡, 所以性能更优
         UnionFindTestHelper.testUF3(n);
+
+        // UF4虽然相对UF3进行有了优化, 但优化的地方出现的情况较少,
+        // 所以性能更优表现的不明显, 甚至在一些数据下性能会更差
+        UnionFindTestHelper.testUF4(n);
     }
 }
---
+++ UnionFind/UnionFind4.java
@@ -0,0 +1,62 @@
+/**
+ * Created by Zhuang on 2018/4/1.
+ */
+public class UnionFind4 {
+    private int[] parent;
+    private int[] rank;    //rank[i]表示以i为根的集合所表示的树的层数
+    private int count;
+
+    public UnionFind4(int count){
+        parent = new int[count];
+        this.count = count;
+        rank = new int[count];
+        for(int i = 0;i < count ; i ++){
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    public int find(int p){
+        assert p >= 0 && p <count;
+
+        /*while(p != parent[p])
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        return p;*/
+
+        if(p != parent[p])
+            parent[p] = find(parent[p]);
+
+        return parent[p];
+    }
+
+    // 查看元素p和元素q是否所属一个集合
+    // O(h)复杂度, h为树的高度
+    public boolean isConnected(int p,int q){
+        return find(q) == find(q);
+    }
+
+    // 合并元素p和元素q所属的集合
+    // O(h)复杂度, h为树的高度
+    public void unionElements(int p,int q){
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if(pRoot == qRoot){
+            return;
+        }
+
+        // 根据两个元素所在树的元素个数不同判断合并方向
+        // 将元素个数少的集合合并到元素个数多的集合上
+        if(rank[pRoot] < rank[qRoot]){
+            parent[pRoot] = qRoot;
+        }else if(rank[qRoot] < rank[pRoot]){
+            parent[qRoot] = pRoot;
+        }
+        else{
+            parent[pRoot] = qRoot;
+            rank[qRoot] += 1;
+        }
+    }
+
+}
